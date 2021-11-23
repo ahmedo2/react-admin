b19b10b26082d016691d58a88cdad48d9db290b7
Ahmed <oublihi.a@gmail.com>
2021-11-23 13:39:24 +0100

    Featured Info Component & Home Page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
+import "./home.css";
+export default function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <div className="homeWidgets">
+
+      </div>
+    </div>
+  );
+}
