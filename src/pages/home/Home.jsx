f85faf56d0cde717d328fd265ec21973c7127282
Ahmed <oublihi.a@gmail.com>
2021-11-23 19:40:04 +0100

    Chart Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
+import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
+import { userData } from "../../data";
+
+
 export default function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
-      <div className="homeWidgets">
-
-      </div>
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
     </div>
   );
 }
+
