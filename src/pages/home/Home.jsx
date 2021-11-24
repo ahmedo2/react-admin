00198d4773b1f3227355236dd9457b49e05b57c6
Ahmed <oublihi.a@gmail.com>
2021-11-24 19:42:33 +0100

    Latest Transactions Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import FeaturedInfo from "../../components/featuredinfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../data";
 import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg";
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
       <div className="homeWidgets">
         <WidgetSm />
+        <WidgetLg />
       </div>
     </div>
   );
