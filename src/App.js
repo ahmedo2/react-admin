c83611450d7ba3ae4c8659cacb127885e0c3981e
Ahmed <oublihi.a@gmail.com>
2021-12-01 18:48:36 +0100

    New User Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 function App() {
   return (
     <Router>
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" element={<Home/>}/>
           <Route path="/users" element={<UserList/>}/>
           <Route path="/user/:userId" element={<User/>}/>
+          <Route path="/newUser" element={<NewUser/>}/>
         </Routes>
       </div>
     </Router>
