3db5ea4c659bc76d96e5d3db3831c4aca3098273
Ahmed <oublihi.a@gmail.com>
2021-12-02 21:18:47 +0100

    Create, Update Products pages

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 function App() {
   return (
     <Router>
@@ -18,6 +21,9 @@ function App() {
           <Route path="/users" element={<UserList/>}/>
           <Route path="/user/:userId" element={<User/>}/>
           <Route path="/newUser" element={<NewUser/>}/>
+          <Route path="/products" element={<ProductList/>}/>
+          <Route path="/product/:productId" element={<Product/>}/>
+          <Route path="/newProduct" element={<NewProduct/>}/>
         </Routes>
       </div>
     </Router>
